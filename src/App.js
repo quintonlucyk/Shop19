1f02e45fa564a0ba7a6a1d6798d2d9dac70f76cc
Quinton Lucyk <qlucyk@uwaterloo.ca>
2019-01-15 00:34:34 -0500

    search static

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Header from './Components/Header.js';
+import Search from './Components/Search.js';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSearch);
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Search />
       </div>
     );
   }
---
+++ src/Components/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="search">
+        <input />
+        <FontAwesomeIcon icon="search" />
+      </div>
+    );
+  }
+}
+
+export default Search;
