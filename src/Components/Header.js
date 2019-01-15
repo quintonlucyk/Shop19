d575c42dc4dfbf4cbcd9f38677655bbd462118aa
Quinton Lucyk <qlucyk@uwaterloo.ca>
2019-01-15 00:01:24 -0500

    header complete

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div id="grad">
+        Toronto Waste Lookup
+      </div>
+    );
+  }
+}
+
+export default Header;
